7ca87f1842f6947614d6f73174ef85187d87045d
Glitch (starter-cra-and-express) <none>
2022-02-01 23:21:26 +0000

    🏬🚂 Checkpoint ./public/index.html:41781219/80 ./src/App.js:41781219/594

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React,  {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {loadStytch} from '@stytch/stytch-js'
 
-const Stytch = loadStytch();
+const StytchPromise = loadStytch().then(Stytch => Stytch('public-token-live-5691c5a7-863e-4241-be93-056ee0756672'));
+
+const useStytch = () => {
+  const [s, setS] = useState()
+  useEffect(() => { StytchPromise.then(s => setS(s)) })
+  return s
+}
 
 function App() {
+  
+  const stytch = useStytch();
+  console.log(stytch);
   
   return (
     <div className="App">
@@ -14,14 +23,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and your changes will live-update automatically.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => stytch.client.magicLinks.email.send("max@stytch.com")}> email </button>
       </header>
     </div>
   );
