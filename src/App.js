0570c1e3c81991bf4176c99fe199e402a31c7003
Glitch (starter-cra-and-express) <none>
2022-02-02 03:02:37 +0000

    🗃🍳 Checkpoint ./src/App.js:41781219/2921

--- src/App.js
+++ src/App.js
@@ -6,20 +6,19 @@ const stytch = window.Stytch(
   "public-token-live-5691c5a7-863e-4241-be93-056ee0756672"
 );
 
-
 function EmailLogin() {
-
   const emailRef = useRef();
-  
-    const onSubmit = React.useCallback(() => {
+
+  const onSubmit = (e) => {
+    e.preventDefault();
     stytch.client.magicLinks.email.send({
-      email: emailRef.current,
+      email: emailRef.current.value,
       signup_magic_link_url: "https://kindhearted-longing-woodpecker.glitch.me",
       login_magic_link_url: "https://kindhearted-longing-woodpecker.glitch.me",
     });
-  }, [stytch, emailRef]);
-  
-    return (
+  };
+
+  return (
     <form onSubmit={onSubmit}>
       <input type="email" name="email" ref={emailRef} required />
       <input type="submit" value="Submit" />
@@ -28,28 +27,30 @@ function EmailLogin() {
 }
 
 function App() {
-  const [loggedIn, setLoggedIn] = useState("maybe");
+  const [loggedIn, setLoggedIn] = useState(false);
 
   useEffect(() => {
     stytch.client.sessions
       .authenticate()
-      .then(data => setLoggedIn(data))
-    .catch(() => setLoggedIn(false));
+      .then((data) => setLoggedIn(true))
+      .catch(() => setLoggedIn(false));
   }, []);
   
-  const content = loggedIn ? ''
+  useEffect(() => {
+    new URL(window.location.href).query
+  }, [])
 
-
+  const content = loggedIn ? (
+    "You are logged in. Good for you, pal."
+  ) : (
+    <EmailLogin />
+  );
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-  
-          {loggedIn === "maybe" ? "loading...." : "You are " + (loggedIn ? "Logged in!" : "Not Logged In!")}
-        </p>
-
+        <p>{content}</p>
       </header>
     </div>
   );
