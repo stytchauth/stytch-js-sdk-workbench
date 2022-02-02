407e4786240f7b9c27c551d38ff246d55e703409
Glitch (starter-cra-and-express) <none>
2022-02-02 02:38:35 +0000

    🏇⛄️ Checkpoint ./src/App.js:41781219/319 ./public/index.html:41781219/89

--- src/App.js
+++ src/App.js
@@ -2,21 +2,16 @@ import React,  {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
-const stytch = Stytch('public-token-live-5691c5a7-863e-4241-be93-056ee0756672')
+const stytch = window.Stytch('public-token-live-5691c5a7-863e-4241-be93-056ee0756672')
 
 
 
 function App() {
-  
-  const stytch = useStytch();
-  console.log(stytch);
-  
   const sendEmail = React.useCallback(() => {
      stytch.client.magicLinks.email.send({
         email: "max@stytch.com",
-        signup_magic_link_url: "https://sdk-app.max.dev.stytch.com/workbench",
-        login_magic_link_url: "https://sdk-app.max.dev.stytch.com/workbench",
+        signup_magic_link_url: "https://kindhearted-longing-woodpecker.glitch.me",
+        login_magic_link_url: "https://kindhearted-longing-woodpecker.glitch.me",
       });
   }, [stytch])
   
