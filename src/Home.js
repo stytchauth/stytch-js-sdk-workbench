ced5dec98fc17c93b72094ee3974b3c8804d7bbd
Glitch (starter-cra-and-express) <none>
2022-02-10 03:14:46 +0000

    🔔🏔 Checkpoint ./src/Home.js:41781219/6969

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import stytch from './Stytch';
 
 const LoggedOut = () => {
   return (
     <div>
       <h2>Welcome to the Headless SDK Demo! </h2>
-      <Link class="Button" to="/login">Login</Link>
+      <Link class="Button" to="/login">
+        Login
+      </Link>
     </div>
   );
 };
@@ -14,8 +17,27 @@ const LoggedIn = () => {
   return <h2>Welcome back! 🎉</h2>;
 };
 
-const Home = ({ authenticated }) => {
-  return authenticated ? <LoggedIn /> : <LoggedOut />;
+const Home = ({ user, session }) => {
+  return (
+    <div class="container">
+      <div class="column">
+        <h1>SDK Workbench</h1>
+
+        <button onClick ={() => stytch.user.registerWebauthn()}>
+          <code>stytch.user.registerWebauthn()</code>
+        </button>
+        <br />
+        <button onClick ={() => stytch.webauthn.authenticate()}>
+          <code>stytch.webauthn.authenticate()</code>
+        </button>
+        <br />
+        <button onClick ={() => stytch.session.revoke()}>
+          <code>stytchSDK.session.revoke() (logout)</code>
+        </button>
+        <br />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
