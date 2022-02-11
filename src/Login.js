05f3814bef8771a65a3daec2ff5a081bf418479b
Glitch (starter-cra-and-express) <none>
2022-02-11 21:06:23 +0000

    🎩😬 Checkpoint ./src/Login.js:41781219/7880

--- src/Login.js
+++ src/Login.js
@@ -1,52 +1,42 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { Stytch } from "@stytch/stytch-react";
 import { useNavigate } from "react-router-dom";
-import { useStytchUser } from './Stytch';
+import { stytch, useStytchUser } from "./Stytch";
 
 const STYTCH_PUBLIC_TOKEN = process.env.REACT_APP_STYTCH_PUBLIC_TOKEN;
 
 const Login = () => {
-  
   const navigate = useNavigate();
   const stytchUser = useStytchUser();
-  
+
   useEffect(() => {
-    if(stytchUser) {
-      navigate('/home')
+    if (stytchUser) {
+      navigate("/home");
     }
-  }, [stytchUser])
-  
-  const stytchProps = {
-    loginOrSignupView: {
-      products: ['oauth', 'emailMagicLinks'],
-      oauthOptions:{
-        providers:[{type:"google"}],
-        loginRedirectURL: "https://kindhearted-longing-woodpecker.glitch.me/authenticate?type=oauth",
-        signupRedirectURL: "https://kindhearted-longing-woodpecker.glitch.me/authenticate?type=oauth",
-      },
-      emailMagicLinksOptions: {
-        loginRedirectURL: "https://kindhearted-longing-woodpecker.glitch.me/authenticate?type=eml",
-        loginExpirationMinutes:30,
-        signupRedirectURL: "https://kindhearted-longing-woodpecker.glitch.me/authenticate?type=eml",
-        signupExpirationMinutes: 30,
-      }
-    },
-    style: {
-      fontFamily: '"Helvetica New", Helvetica, sans-serif',
-      width: "321px",
-      primaryColor: "#0577CA",
-      primaryTextColor: "#090909",
-    },
-    publicToken: STYTCH_PUBLIC_TOKEN
+  }, [stytchUser]);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target, e.target.value);
+    stytch.magicLinks.email.loginOrCreate(1, {
+      loginRedirectURL:
+        "https://kindhearted-longing-woodpecker.glitch.me/authenticate?type=eml",
+      loginExpirationMinutes: 30,
+      signupRedirectURL:
+        "https://kindhearted-longing-woodpecker.glitch.me/authenticate?type=eml",
+      signupExpirationMinutes: 30,
+    });
   };
 
   return (
     <div className="Sign-in-container">
-      <Stytch
-        publicToken={stytchProps.publicToken}
-        loginOrSignupView={stytchProps.loginOrSignupView}
-        style={stytchProps.style}
-      />
+      <form onSubmit={onSubmit}>
+        <label>
+          Email:
+          <input name="email" type="email" />
+        </label>
+        <button type="submit"> Submit </button>
+      </form>
     </div>
   );
 };
